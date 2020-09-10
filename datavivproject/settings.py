c9bab29ab8bb8566094e60b674ae4e38637228d2
root <root@camerax-instance.us-central1-a.c.brave-computer-288608.internal>
2020-09-10 10:27:32 +0000

    testuser added

--- datavivproject/settings.py
+++ datavivproject/settings.py
@@ -173,5 +173,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vedant.dataviv@gmail.com'
-EMAIL_HOST_PASSWORD = 'Dataviv@123'
+EMAIL_HOST_PASSWORD = ''
 
